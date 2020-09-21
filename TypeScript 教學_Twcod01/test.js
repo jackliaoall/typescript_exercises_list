f4d2145dfceab7b0f8214f6b0b4a679f8637f991
jackliaoall <jackliaoall@gmail.com>
2020-09-21 13:15:46 +0800

    Delete test.js

--- TypeScript 教學_Twcod01/test.js
+++
@@ -1,9 +0,0 @@
-function employee(id, name) {
-    this.id = id;
-    this.name = name;
-}
-var emp = new employee(123, "admin");
-employee.prototype.email = "test@mail";
-console.log("員工號: " + emp.id);
-console.log("員工姓名: " + emp.name);
-console.log("員工郵箱: " + emp['email']);
